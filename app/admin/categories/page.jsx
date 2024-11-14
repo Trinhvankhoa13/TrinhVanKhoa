39e0db128fef63435fed7c892f00712269cbc32b
Trinhvankhoa@2003 <Trinhvankhoa13>
2024-11-15 00:59:37 +0700

    d

--- app/admin/categories/page.jsx
+++ app/admin/categories/page.jsx
@@ -1,3 +1,9 @@
-export default function page(params) {
-    
+import Form from "./components/Form";
+
+export default function Page() {
+    return (
+        <main className="p-5">
+            <Form/>
+        </main>
+    )
 };
